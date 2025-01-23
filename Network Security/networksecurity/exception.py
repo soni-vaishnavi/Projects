a177b3c603d11ab32702244d8a0809400ceae861
VaishnaviInfinity <vaishnavisoni1723@gmail.com>
2025-01-24 00:07:15 +0530

    mssg

--- Network Security/networksecurity/exception.py
+++ Network Security/networksecurity/exception.py
@@ -11,6 +11,8 @@ class NetworkSecurityException(Exception):
     def __str__(self):
         return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
             self.file_name, self.lineno, str(self.error_message)
+
+            
         )
         
 if __name__=='__main__':
